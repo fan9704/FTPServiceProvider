2fddd2768597bc51f951a28a08a2073c3ada6eac
fan9704 <cxz123499@gmail.com>
2022-06-24 00:20:07 +0800

    Complete Upload File Template

--- src/main/java/com/mkyong/SpringBootWebApplication.java
+++ src/main/java/com/mkyong/SpringBootWebApplication.java
@@ -1,6 +1,5 @@
 package com.mkyong;
 
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
@@ -13,7 +12,6 @@ public class SpringBootWebApplication {
 
     public static void main(String[] args) throws Exception {
         new SpringApplicationBuilder(SpringBootWebApplication.class)
-                .web(WebApplicationType.NONE)
                 .run(args);
 
     }
