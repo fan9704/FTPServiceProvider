60edb23f40b5db264f06e84074ea777735e50ea0
fan9704 <cxz123499@gmail.com>
2022-07-09 09:56:36 +0800

    Create GlobalCorConfiguration

---
+++ src/main/java/com/mkyong/configuration/GlobalCorsConfiguration.java
@@ -0,0 +1,25 @@
+package com.mkyong.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class GlobalCorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+//                        .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS","HEAD")
+                .allowedHeaders("*")
+                .allowedOriginPatterns("*");
+    }
+
+}
